acd2f326a8c4aad71b63c466c1a50b80948bf733
AnnaKuvarina <anna.kuvarina@sigma.software>
2021-12-14 13:42:35 +0200

    implement spin lock

---
+++ spinning_locks/multi_ledger_usin_mutex.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+func performMovementsWithMutex(ledger *[totalAccounts]int32, locks *[totalAccounts]sync.Locker, totalTrans *int64) {
+	for {
+		accountA := rand.Intn(totalAccounts)
+		accountB := rand.Intn(totalAccounts)
+		for accountA == accountB {
+			accountB = rand.Intn(totalAccounts)
+		}
+
+		amountToMove := rand.Int31n(maxAmountMoved)
+
+		// to ensure there is no deadlocks we sort array
+		toLock := []int{accountA, accountB}
+		sort.Ints(toLock)
+
+		locks[toLock[0]].Lock()
+		locks[toLock[1]].Lock()
+
+		atomic.AddInt32(&ledger[accountA], -amountToMove)
+		atomic.AddInt32(&ledger[accountB], amountToMove)
+		atomic.AddInt64(totalTrans, 1)
+
+		locks[toLock[1]].Unlock()
+		locks[toLock[0]].Unlock()
+	}
+}
+
+func main() {
+	fmt.Printf("Total accounts: %d, total threads: %d using Mutexes\n", totalAccounts, threads)
+	var ledger [totalAccounts]int32
+	var locks [totalAccounts]sync.Locker
+	var totalTrans int64
+
+	for i := 0; i < totalAccounts; i++ {
+		ledger[i] = initialMoney
+		locks[i] = &sync.Mutex{}
+	}
+
+	for i := 0; i < threads; i++ {
+		go performMovementsWithMutex(&ledger, &locks, &totalTrans)
+	}
+
+	for {
+		time.Sleep(2000 * time.Millisecond)
+		var sum int32
+
+		for i := 0; i < totalAccounts; i++ {
+			locks[i].Lock()
+		}
+
+		for i := 0; i < totalAccounts; i++ {
+			sum += ledger[i]
+		}
+		for i := 0; i < totalAccounts; i++ {
+			locks[i].Unlock()
+		}
+
+		fmt.Printf("Total trans: %d, sum: %d \n", totalTrans, sum)
+	}
+}
